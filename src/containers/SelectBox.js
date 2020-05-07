41416856c99a6ba64b346222f3100a00c5a8cd49
Taha <tahaselimaksakal@gmail.com>
2020-05-07 23:52:12 +0300

    set default value for selectbox

--- src/containers/SelectBox.js
+++ src/containers/SelectBox.js
@@ -3,12 +3,11 @@ import { SelectBoxs } from '../layout/styles'
 
 export default function SelectBox(props) {
     return (
-        <SelectBoxs onChange={(e) => props.setType(e.target.value)}>
+        <SelectBoxs onChange={(e) => props.setType(e.target.value)} defaultValue={''}>
         {
             props.sortOptions.map((o,index) => {
             return (<option 
                 disabled={o.disabled}
-                selected={o.selected}
                 key={index}
                 value={o.value}>
                 {o.label}
--- src/helpers/constant.js
+++ src/helpers/constant.js
@@ -1,19 +1,16 @@
 export let sortOptions = [
     {
         disabled: true,
-        selected: true,
         value: '',
         label: 'Order By Vote'
     },
     {
         disabled: false,
-        selected: false,
         value: 'asc',
         label: 'Most voted'
     },
     {
         disabled: false,
-        selected: false,
         value: 'desc',
         label: 'Less voted'
     }
